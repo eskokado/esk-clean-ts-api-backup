616545ad5a2901e1c9549e86ff979aeb1adb4468
Edson Shideki Kokado <eskokado@gmail.com>
2021-01-23 17:09:53 -0300

    refactor: group middleware interfaces in a single file

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,7 +1,10 @@
 module.exports = {
+  mongodbMemoryServer: {
+    version: '6.5.1'
+  },
   mongodbMemoryServerOptions: {
     binary: {
-      version: '4.0.3',
+      version: '4.0.14',
       skipMD5: true
     },
     autoStart: false,
