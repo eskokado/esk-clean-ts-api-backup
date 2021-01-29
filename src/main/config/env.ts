bb3899f98efb1e993994ce7f3b5ae618bad559cc
Edson Shideki Kokado <eskokado@gmail.com>
2021-01-29 08:16:35 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/esk-clean-node-api',
-  port: process.env.PORT || 8088
+  port: process.env.PORT || 8088,
+  jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
